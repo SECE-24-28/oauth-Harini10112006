59fc5e26ecf259960e9be1bc438cebd626f65ab5
K.Harini <harinikaruppiah06@gmail.com>
2026-05-27 09:28:22 +0530

    Day_2 Updated

--- src/main/java/com/eduhub/eduhub_backend/controller/CourseController.java
+++ src/main/java/com/eduhub/eduhub_backend/controller/CourseController.java
@@ -1,27 +1,63 @@
 package com.eduhub.eduhub_backend.controller;
 
 import com.eduhub.eduhub_backend.component.CourseService;
-import com.eduhub.eduhub_backend.component.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eduhub.eduhub_backend.component.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CourseController {
-    @Autowired
-    CourseService courseService;
 
-    @GetMapping("get-course")
-    public String getCourse(){
-        return courseService.getCourse();
+    @GetMapping("courses")
+    public ResponseEntity<List<CourseService>> getCourse(){
+        List<CourseService> coursesList=new ArrayList<>();
+        coursesList.add(new CourseService("cs11","OS",4));
+        coursesList.add(new CourseService("cs12","JAVA",3));
+        coursesList.add(new CourseService("cs13","DBMS",4));
+        coursesList.add(new CourseService("cs14","Python",2));
+        return new ResponseEntity<>(coursesList,HttpStatus.OK);
     }
 
-    @Autowired
-    DemoService demoService;
-
-    @GetMapping("get-demo")
-    public String getDemo(){
-        return demoService.getDemo();
+    //http://localhost:8080/id/firstname/lastname
+    @GetMapping("{courseCode}")
+    public ResponseEntity<CourseService> coursePathVariable(@PathVariable("courseCode") String courseCode)
+    {
+        CourseService courseService = new CourseService();
+        return new ResponseEntity<>(courseService, HttpStatus.OK);
     }
 
+    //http://localhost:8080/query?studentId=2&firstName=Visahasri&lastName=G
+    @GetMapping("query")
+    public ResponseEntity<CourseService> courseServiceResponseEntity(@RequestParam String courseCode,
+                                                           @RequestParam String subjectName,
+                                                           @RequestParam int credits
+    )
+    {
+        CourseService courseService = new CourseService(courseCode, subjectName, credits);
+        return ResponseEntity.ok(courseService);
+    }
+
+    @PostMapping("create-course")
+    public  ResponseEntity<CourseService> createCourse(@RequestBody CourseService courseService){
+        System.out.println(courseService.getCourseCode());
+        System.out.println(courseService.getSubjectName());
+        System.out.println(courseService.getCredits());
+        return ResponseEntity.ok(courseService);
+    }
+
+    @PutMapping("{courseCode}/update-course")
+    public ResponseEntity updateCourse(@PathVariable("courseCode") String courseCode,
+                                        @RequestBody CourseService courseService
+    ){
+        return ResponseEntity.accepted().body(courseCode);
+    }
+
+    @DeleteMapping("{courseCode}/delete")
+    public ResponseEntity deleteCourse(@PathVariable("courseCode") String courseCode){
+        return ResponseEntity.ok(courseCode);
+    }
 }
--- src/main/java/com/eduhub/eduhub_backend/controller/StudentController.java
+++ src/main/java/com/eduhub/eduhub_backend/controller/StudentController.java
@@ -1,29 +1,73 @@
-package com.eduhub.eduhub_backend.controller;
-
-import com.eduhub.eduhub_backend.component.Student;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-public class StudentController {
-    @GetMapping("student")
-    public ResponseEntity<Student>getStudent(){
-        Student student = new Student(1, "Harini", "K");
-        return new ResponseEntity<>(student, HttpStatus.OK);
-    }
-
-    @GetMapping("students")
-    public ResponseEntity<List<Student>> getStudents(){
-        List<Student> studentList=new ArrayList<>();
-        studentList.add(new Student(1,"Sai","Karishma"));
-        studentList.add(new Student(2,"Visahasri","G"));
-        studentList.add(new Student(3,"Abirami","S M"));
-        studentList.add(new Student(4,"Kanishka","S B"));
-        return new ResponseEntity<>(studentList,HttpStatus.OK);
-    }
-}
+//package com.eduhub.eduhub_backend.controller;
+//
+//import com.eduhub.eduhub_backend.component.Student;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//public class StudentController {
+//    @GetMapping("student")
+//    public ResponseEntity<Student>getStudent(){
+//        Student student = new Student(1, "Harini", "K");
+//        return new ResponseEntity<>(student, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("students")
+//    public ResponseEntity<List<Student>> getStudents(){
+//        List<Student> studentList=new ArrayList<>();
+//        studentList.add(new Student(1,"Sai","Karishma"));
+//        studentList.add(new Student(2,"Visahasri","G"));
+//        studentList.add(new Student(3,"Abirami","S M"));
+//        studentList.add(new Student(4,"Kanishka","S B"));
+//        return new ResponseEntity<>(studentList,HttpStatus.OK);
+//    }
+//
+//    //http://localhost:8080/id/firstname/lastname
+//    @GetMapping("{id}/{first-name}/{last-name}")
+//    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
+//                                                       @PathVariable("first-name") String firstName,
+//                                                       @PathVariable("last-name") String lastName
+//                                                       )
+//    {
+//        Student student = new Student(studentId, firstName, lastName);
+//        return new ResponseEntity<>(student, HttpStatus.OK);
+//        //return ResponseEntity.ok(student);
+//    }
+//
+//    //http://localhost:8080/query?studentId=2&firstName=Visahasri&lastName=G
+//    @GetMapping("query")
+//    public ResponseEntity<Student> studentResponseVariable(@RequestParam int studentId,
+//                                                           @RequestParam String firstName,
+//                                                           @RequestParam String lastName
+//                                                           )
+//    {
+//        Student student = new Student(studentId, firstName, lastName);
+//        //return new ResponseEntity<>(student, HttpStatus.OK);
+//        return ResponseEntity.ok(student);
+//    }
+//
+//    @PostMapping("create")
+//    public  ResponseEntity<Student> createStudent(@RequestBody Student student){
+//        System.out.println(student.getId());
+//        System.out.println(student.getFirstName());
+//        System.out.println(student.getLastName());
+//        return ResponseEntity.ok(student);
+//    }
+//
+//    //http://localhost:8080/update
+////      @PutMapping("update")
+////    public ResponseEntity updateStudent(){
+////        return ResponseEntity.badRequest().body("Not found");
+////    }
+//
+//    @PutMapping("{id}/update")
+//    public ResponseEntity updateStudent(@PathVariable("id") int studentId,
+//                                        @RequestBody Student student
+//                                        ){
+//        return ResponseEntity.accepted().body(student);
+//    }
+//} */
